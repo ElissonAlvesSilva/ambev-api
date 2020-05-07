1d3c53d23f13c890c7a3bac22d15d47ccd9d7d86
Elisson Alves Silva <elisson@MacBook-Pro-de-Elisson.local>
2020-05-07 16:44:48 -0400

    refact: refact cost center model

--- models/cost_center.js
+++ models/cost_center.js
@@ -1,32 +1,32 @@
 /* jshint indent: 2 */
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   return sequelize.define('cost_center', {
     id: {
       type: DataTypes.INTEGER(11),
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     kernel_id: {
       type: DataTypes.INTEGER(11),
       allowNull: false,
       references: {
         model: 'kernel',
-        key: 'id'
-      }
+        key: 'id',
+      },
     },
     key: {
       type: DataTypes.STRING(30),
       allowNull: false,
-      unique: true
+      unique: true,
     },
     address: {
       type: DataTypes.TEXT,
-      allowNull: true
-    }
+      allowNull: true,
+    },
   }, {
     timestamps: false,
-    tableName: 'cost_center'
+    tableName: 'cost_center',
   });
 };
